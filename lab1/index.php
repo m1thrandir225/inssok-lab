e4fc40763ba024af2592f9e0caed4a8f72d568ba
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-11-17 10:33:06 +0100

    style: indentation fix

--- lab1/index.php
+++ lab1/index.php
@@ -8,48 +8,50 @@ $students = [
   ["name" => "Marija", "age" => 19, "grades" => array(6, 6, 8)]
 ];
 
-function calculateAverage($grades) {
-  $sum = array_sum($grades);
+function calculateAverage($grades)
+{
+    $sum = array_sum($grades);
 
-  return round($sum/count($grades), 2);
+    return round($sum / count($grades), 2);
 }
 
-function filterByAge($students, $minAge) {
-  $result = [];
+function filterByAge($students, $minAge)
+{
+    $result = [];
 
-  foreach ($students as $student) {
-    if ($student['age'] >= $minAge) {
-      array_push($result, $student);
+    foreach ($students as $student) {
+        if ($student['age'] >= $minAge) {
+            array_push($result, $student);
+        }
     }
-  }
 
-  return $result;
+    return $result;
 }
 
-function capitalizeNames($students) {
-  foreach ($students as $student) {
-    ucfirst($student['name']);
-  }
+function capitalizeNames($students)
+{
+    foreach ($students as $student) {
+        ucfirst($student['name']);
+    }
 }
 
-function displayStudents($students) {
-  foreach ($students as $student) {
-    print("Name: " . $student["name"] . ", Age: " . $student["age"] . ", Average Grade: " . calculateAverage($student["grades"]) . "\n");
-  }
+function displayStudents($students)
+{
+    foreach ($students as $student) {
+        print("Name: " . $student["name"] . ", Age: " . $student["age"] . ", Average Grade: " . calculateAverage($student["grades"]) . "\n");
+    }
 }
 
-function sortByName(&$students) {
-  foreach($students as $student) {
-    lcfirst($student["name"]);
-  }
+function sortByName(&$students)
+{
+    foreach ($students as $student) {
+        lcfirst($student["name"]);
+    }
 
-  sort($students);
+    sort($students);
 }
 
 
 sortByName($students);
 capitalizeNames($students);
-displayStudents($students)
-
-
-?>
+displayStudents($students);
